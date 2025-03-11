d1e780e39386f18a913dd8f54d7702102af5f03f
Fabio Insolia <fabio.insolia@checkout.com>
2025-03-11 23:16:37 +0000

    Solution
    Little tweeks
    
    Moved isAvailable check in VM, removed coroutinescope in favour of lauch or it could leak if not cancelled
    
    Removing coroutine scope in favour of viewmodelscope to prevent leaks
    
    feature/PIMOB-3416_Update_steps_in_solution_branch
    
    Updating the text button
    
    Adding processing channel id via build config
    
    Ktlint
    
    reverting name onShowPaymentComponentButtonClick

--- app/src/main/java/com/example/googlepaylive/SampleScreen.kt
+++ app/src/main/java/com/example/googlepaylive/SampleScreen.kt
@@ -15,15 +15,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.checkout.components.interfaces.api.PaymentMethodComponent
 import com.example.googlepaylive.screen.PaymentUiState
 
 @Composable
 internal fun SampleScreen(viewModel: SampleViewModel = hiltViewModel<SampleViewModel>()) {
+    // Step 6-2  adding the component and isAvailable to the SampleScreen
+    val checkoutComponent = viewModel.component.collectAsState()
+    val isAvailable = viewModel.isAvailable.collectAsState()
+
     val context = LocalContext.current
     val uiState = viewModel.paymentSessionState.collectAsState()
     SampleScreen(
         { viewModel.renderFlow(context) },
         uiState,
+        // Step 6-3
+        checkoutComponent,
+        isAvailable,
     )
 }
 
@@ -31,6 +39,9 @@ internal fun SampleScreen(viewModel: SampleViewModel = hiltViewModel<SampleViewM
 internal fun SampleScreen(
     onShowPaymentComponentButtonClick: () -> Unit,
     uiState: State<PaymentUiState>,
+    // Step 6-4
+    component: State<PaymentMethodComponent?>,
+    isAvailable: State<Boolean>,
 ) {
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -52,6 +63,10 @@ internal fun SampleScreen(
                     text = "Error: $it",
                     textAlign = TextAlign.Center,
                 )
+            }
+            // Step 6-5
+            if (isAvailable.value) {
+                component.value?.Render()
             }
         }
     }
