57ae912d7c85fe31c0555d12e6213ed53c8d3d40
Fabio Insolia <fabio.insolia@checkout.com>
2025-03-11 23:16:37 +0000

    Solution

--- app/src/main/java/com/example/googlepaylive/SampleActivity.kt
+++ app/src/main/java/com/example/googlepaylive/SampleActivity.kt
@@ -3,12 +3,30 @@ package com.example.googlepaylive
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import com.checkout.components.wallet.wrapper.GooglePayFlowCoordinator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SampleActivity : ComponentActivity() {
+    // Step 9 wallet is not inflating beacuse is expecting the coordinator
+    private val viewModel: SampleViewModel by viewModels()
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Step 9-1 add this here THEN move to the VM
+        val googlePayFlowCoordinator = GooglePayFlowCoordinator(
+            context = this,
+            handleActivityResult = { resultCode, data ->
+                viewModel.handleActivityResult(resultCode, data)
+            },
+        )
+
+        viewModel.setFlowCoordinator(googlePayFlowCoordinator)
+
         setContent {
             SampleScreen()
         }
--- app/src/main/java/com/example/googlepaylive/SampleScreen.kt
+++ app/src/main/java/com/example/googlepaylive/SampleScreen.kt
@@ -8,30 +8,49 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.checkout.components.interfaces.api.PaymentMethodComponent
 import com.example.googlepaylive.screen.PaymentUiState
 
 @Composable
 internal fun SampleScreen(viewModel: SampleViewModel = hiltViewModel<SampleViewModel>()) {
+    // Step 8 adding the component to the SampleScreen
+    val checkoutComponent = viewModel.component.collectAsState()
+
     val context = LocalContext.current
     val uiState = viewModel.paymentSessionState.collectAsState()
     SampleScreen(
+        // Step 1-2
+//        viewModel::createPaymentSession,
         { viewModel.renderFlow(context) },
         uiState,
-    )
+        // Step 8-2
+        checkoutComponent,
+
+        )
 }
 
 @Composable
 internal fun SampleScreen(
-    onShowPaymentComponentButtonClick: () -> Unit,
+    onCreatePaymentSessionButtonClick: () -> Unit,
     uiState: State<PaymentUiState>,
+    // Step 8-3
+    component: State<PaymentMethodComponent?>,
 ) {
+    // Step 8-4
+    var isAvailable by remember { mutableStateOf(false) }
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background,
@@ -41,8 +60,8 @@ internal fun SampleScreen(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = onShowPaymentComponentButtonClick) {
-                Text(text = "Show Payment Component")
+            Button(onClick = onCreatePaymentSessionButtonClick) {
+                Text(text = "Create Payment Session")
             }
             if (uiState.value.isLoading) {
                 Text("Loading...")
@@ -52,6 +71,16 @@ internal fun SampleScreen(
                     text = "Error: $it",
                     textAlign = TextAlign.Center,
                 )
+            }
+            // Step 8-6
+            component.value?.let {
+                LaunchedEffect(component) {
+                    isAvailable = it.isAvailable()
+                }
+                if (isAvailable) {
+                    it.Render()
+                }
+
             }
         }
     }
